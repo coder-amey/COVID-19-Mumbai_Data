d90cb9fd0940630789f3636ffd0e7a8b28dec328
Amey Noolkar <fb4amey@gmail.com>
2021-08-01 17:23:11 +0530

    [BUGFIX] Workaround for slow download of CSV file.

--- update_scripts/fetch_update.py
+++ update_scripts/fetch_update.py
@@ -15,9 +15,10 @@ def compute_predictions(matrix, pred_index):
 base_dir = os.path.join(os.path.dirname(__file__), "../")		#Obtain the path to the base directory for absosulte addressing.
 
 #Tabulate the Worksheet data.
-raw_data = requests.get('https://docs.google.com/spreadsheet/ccc?key=1-OYukZzMlRcRKfMqh-pAle0JYAjLjUy08N9V6S51sq0&output=csv')
-raw_data = raw_data.content.decode().split('\r\n')
-time_series = data.DataFrame([row.split(',') for row in raw_data[1:]], columns = raw_data[0].split(','))
+time_series = data.read_csv(base_dir + "update_scripts/Mumbai COVID-19 Daily Statistics - Daily Statistics.csv")
+#raw_data = requests.get('https://docs.google.com/spreadsheet/ccc?key=1-OYukZzMlRcRKfMqh-pAle0JYAjLjUy08N9V6S51sq0&output=csv')
+#raw_data = raw_data.content.decode().split('\r\n')
+#time_series = data.DataFrame([row.split(',') for row in raw_data[1:]], columns = raw_data[0].split(','))
 
 #Rename and format the required columns and drop the rest.
 time_series = time_series.rename(columns = {"Cumulative Cases": "Confirmed", "Cumulative Recovered": "Recovered", "Cumulative Deaths": "Deceased"})
